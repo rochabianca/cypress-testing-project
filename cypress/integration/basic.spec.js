c12af62b670edeebfdecd9d0c6f7df7863a0cc24
rochabianca <bianca.rocha1711@gmail.com>
2019-10-31 10:51:35 -0300

    aliases

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,9 +1,12 @@
 describe('My first cypress test', () => {
 	it('should load example webpage', function() {
-		cy.visit('http://example.com/')
+		cy.visit('/')
 
-		cy.url().should('include', 'http://example.com/')
+		cy.url().should('include', '/')
 		cy.title().should('eq', 'Example Domain')
-		cy.get('h1').contains('Example Domain')
+
+		cy.get('h1').as('title')
+		cy.get('@title').contains('Example Domain')
+		cy.get('@title').should('be.visible')
 	})
 })
