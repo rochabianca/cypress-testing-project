c32bfb7da7556002dc0b86cc0c6a3c2b3bde3f78
rochabianca <bianca.rocha1711@gmail.com>
2019-12-24 10:51:06 -0300

    alias

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -20,4 +20,19 @@ describe('Action Test', () => {
     cy.get('.result-content').should('be.visible')
     cy.url().should('include', 'thank-you')
   })
+  it('should enable slider', function () {
+    cy.get('#slider').as('Slider')
+
+    cy.get('@Slider').should('have.class', 'ui-state-disabled')
+    cy.get('#tried-test-cafe').click()
+    cy.get('@Slider').should('not.have.class', 'ui-state-disabled')
+  })
+  it('should have empty value by default', function () {
+    cy.get('#comments').as('Textarea')
+
+    cy.get('#tried-test-cafe').click()
+    cy.get('@Textarea').should('have.value', '')
+    cy.get('@Textarea').type('new value')
+    cy.get('@Textarea').should('have.value', 'new value')
+  })
 })
