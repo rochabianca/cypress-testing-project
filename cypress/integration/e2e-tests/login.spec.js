3c060bcc7780769d479efe797bee148e1934e29a
rochabianca <bianca.rocha1711@gmail.com>
2020-01-06 15:38:01 -0300

    login and logout tests

--- cypress/integration/e2e-tests/login.spec.js
+++ cypress/integration/e2e-tests/login.spec.js
@@ -1,4 +1,4 @@
-describe('Login Test', () => {
+describe('Login Failed Test', () => {
   before(function () {
     cy.visit('http://zero.webappsecurity.com/index.html')
     cy.url().should('include', "index.html")
@@ -16,5 +16,36 @@ describe('Login Test', () => {
   it('should display error message', function () {
     cy.get('.alert-error').as('Error_Message')
     cy.get('@Error_Message').should('be.visible')
+  })
+});
+
+describe('Login Success Test', () => {
+  before(function () {
+    cy.visit('http://zero.webappsecurity.com/index.html')
+    cy.url().should('include', "index.html")
+  })
+
+  it('should display login form', function () {
+    cy.get('#signin_button').click()
+    cy.get('#login_form').should('be.visible')
+  })
+
+  it('should login to application', function () {
+    cy.loginToApp("username", "password")
+  })
+
+  it('should display navbar links after login', function () {
+    cy.get('#account_summary_tab').should('be.visible')
+    cy.get('#account_activity_tab').should('be.visible')
+    cy.get('#transfer_funds_tab').should('be.visible')
+    cy.get('#pay_bills_tab').should('be.visible')
+    cy.get('#money_map_tab').should('be.visible')
+    cy.get('#online_statements_tab').should('be.visible')
+  })
+
+  it('should loggout from application', function () {
+    cy.get('.icon-user').click()
+    cy.get('#logout_link').click()
+    cy.get('#homeMenu').should('be.visible')
   })
 });
